347755aa2c6c280a118005dd06b13a43f5496460
Jakub Konka <jakub.konka@strath.ac.uk>
2015-09-27 11:42:48 +0100

    Move pattern matching to the top

--- chattify.js
+++ chattify.js
@@ -1,18 +1,19 @@
 function filterF(i, el) {
-  var pattern = /(From|To|Subject|Cc|Date):/i;
+  var pattern = /(From|To|Sent|Subject|Cc|Date):/i;
   return el.textContent.match(pattern);
 }
 
+// convert From...To... blocks into one-liners
+var matches = $(document).find("*").filter(filterF).get().reverse();
+
+while (matches.length > 0) {
+  matches[0].remove();
+  matches = $(document).find("*").filter(filterF).get().reverse();
+}
+
+// remove blockquotes but preserve contents
 $($("blockquote").get().reverse()).each(function(i, el) {
   var contents = $(el).contents();
-  var matches = contents.find("*").filter(filterF).get().reverse();
-
-  while (matches.length > 0) {
-    matches[0].remove();
-    matches = contents.find("*").filter(filterF).get().reverse();
-  }
-
-  // remove blockquotes but preserve contents
   contents.insertAfter($(el));
   $(el).remove();
 });
