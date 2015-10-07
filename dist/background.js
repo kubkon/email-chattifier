b09460c7b2126eecf6091f3aa63a8d8b1cf41129
Jakub Konka <kubkon@jakubkonka.com>
2015-10-07 18:37:29 +0100

    Change browserAction to pageAction, and change name to GMail Chattifier

--- dist/background.js
+++ dist/background.js
@@ -1,3 +1,9 @@
-chrome.browserAction.onClicked.addListener(function(_) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url.indexOf('https://mail.google.com') == 0) {
+    chrome.pageAction.show(tabId);
+  }
+});
+
+chrome.pageAction.onClicked.addListener(function(_) {
   chrome.tabs.executeScript(null, { file: "bundle.js" });
 });
